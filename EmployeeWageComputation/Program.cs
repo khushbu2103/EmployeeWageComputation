4ceb8fe6371082f5190fbfef45e8301484a4348f
khushbu <khushbusoni475@gmail.com>
2023-05-10 23:48:54 +0530

    [khushbu] Modify Employee Wage

--- EmployeeWageComputation/Program.cs
+++ EmployeeWageComputation/Program.cs
@@ -4,18 +4,24 @@
     {
         static void Main(string[] args)
         {
-            const int IS_PRESENT = 1;
+            const int FULL_TIME = 1;
+            const int EMP_RATE_PER_HR = 20;
+            int empHrs = 0;
+            int empWage = 0;
             Console.WriteLine("Welcome to EmployeeWageComputation");
             Random random = new Random();
             int randomInput=random.Next(0, 2);
-            if (IS_PRESENT==randomInput)
+            if (FULL_TIME==randomInput)
             {
                 Console.WriteLine("Employee is present");
+                empHrs = 8;
             }
             else
             {
                 Console.WriteLine("Employee is absent");
             }
+            empWage = EMP_RATE_PER_HR * empHrs;
+            Console.WriteLine("EmployeeWage:{0}",empWage);
             Console.ReadLine();
         }
     }
