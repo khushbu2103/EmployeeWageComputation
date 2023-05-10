3190f934c621fedbe270c63b0db5ebfd49886a0b
khushbu <khushbusoni475@gmail.com>
2023-05-10 10:49:33 +0530

    [khushbu] Adding Welcome Message

---
+++ EmployeeWageComputation/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace EmployeeWageComputation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to EmployeeWageComputation");
+            Console.ReadLine();
+        }
+    }
+}
