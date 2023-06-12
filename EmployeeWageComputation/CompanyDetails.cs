ff3ab8f72e4873054d873f74a29b9ad18db1ba5d
khushbu <khushbusoni475@gmail.com>
2023-06-13 01:09:20 +0530

    [khushbu] modify manage Employee Wage of multiple companies

---
+++ EmployeeWageComputation/CompanyDetails.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWageComputation
+{
+    internal class CompanyDetails
+    {
+        public string company;
+        public int maxWorkingDay, maxWorkingHrs, empRatePerHr, totalWage;
+
+        public CompanyDetails(string company, int maxWorkingDay, int maxWorkingHrs, int empRatePerHr)
+        {
+            this.company = company;
+            this.maxWorkingDay = maxWorkingDay;
+            this.maxWorkingHrs = maxWorkingHrs;
+            this.empRatePerHr = empRatePerHr;
+        }
+        
+        public void SetTotalWage(int totalWage)
+        {
+            this.totalWage=totalWage;
+        }
+        public override string ToString()
+        {
+            return $" comapny:{ company} maxdays:{maxWorkingDay} maxhrs:{maxWorkingHrs} rateperhr:{empRatePerHr} totalwage:{totalWage} ";
+        }
+    }
+}
--- EmployeeWageComputation/EmpWageBuilder.cs
+++ EmployeeWageComputation/EmpWageBuilder.cs
@@ -4,18 +4,34 @@ namespace EmployeeWageComputation
 {
     internal class EmpWageBuilder
     {
-        public string company;
-        public int maxWorkingDay, maxWorkingHrs, empRatePerHr;
+        //int[] arr = new int[5];
+        public CompanyDetails[] companies;
+        int noOfCompanies = 0;
 
-        public EmpWageBuilder(string company, int maxWorkingDay, int maxWorkingHrs, int empRatePerHr)
+        public EmpWageBuilder()
         {
-            this.company = company;
-            this.maxWorkingDay = maxWorkingDay;
-            this.maxWorkingHrs = maxWorkingHrs;
-            this.empRatePerHr = empRatePerHr;
+            companies = new CompanyDetails[5];
         }
 
-            public void CalculateWage()
+        public void AddCompanyDetails(string company, int maxWorkingDay, int maxWorkingHrs, int empRatePerHr)
+        {
+            CompanyDetails comp = new CompanyDetails(company, maxWorkingDay, maxWorkingHrs, empRatePerHr);
+            companies[noOfCompanies] = comp;
+            noOfCompanies++;
+        }
+        public void IterateOverCompanies()
+        {
+            for (int i = 0; i < companies.Length; i++)
+            {
+                if (companies[i] != null)
+                {
+                    int totalWage = CalculateWage(companies[i]);
+                    companies[i].SetTotalWage(totalWage);
+                    Console.WriteLine(companies[i]);
+                }
+            }
+        }
+            public int CalculateWage(CompanyDetails obj)
             {
             const int FULL_TIME = 1;
             const int PART_TIME = 2;
@@ -26,7 +42,7 @@ namespace EmployeeWageComputation
             int day = 1;
             int totalHrs = 0;
                 Random random = new Random();
-                while (day <= maxWorkingDay && totalHrs <= maxWorkingHrs)
+                while (day <= obj.maxWorkingDay && totalHrs <= obj.maxWorkingHrs)
                 {
 
                     int randomInput = random.Next(0, 3);
@@ -46,25 +62,24 @@ namespace EmployeeWageComputation
                             //code block;
                             break;
                     }
-                    empWage = empRatePerHr * empHrs;
+                    empWage = obj.empRatePerHr * empHrs;
                     totalWage += empWage; //totalWage=totalWage+emoWage
                     totalHrs += empHrs;
                     //Console.WriteLine("EmployeeWage per day{0}:{1}", day, empWage);
                     day++;
                 }
-                Console.WriteLine("TotalWage {0} for {1} of {2} days and totalhrs:{3}", totalWage, company, (day-1), totalHrs);
-                
+                //Console.WriteLine("TotalWage {0} for {1} of {2} days and totalhrs:{3}", totalWage, obj.company, (day-1), totalHrs);
+            return totalWage;
             }
 
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to EmployeeWageComputation");
-            EmpWageBuilder deloitte = new EmpWageBuilder("Deloitte", 24, 120, 40);
-            deloitte.CalculateWage();
-            EmpWageBuilder Microsoft = new EmpWageBuilder("Microsoft", 34, 100, 46);
-            deloitte.CalculateWage();
-            EmpWageBuilder Bridgelabz = new EmpWageBuilder("Bridgelabz", 20, 130, 35);
-            deloitte.CalculateWage();
+            EmpWageBuilder empWageBuilder = new EmpWageBuilder();
+            empWageBuilder.AddCompanyDetails("Deloitte", 24, 120, 40);
+            empWageBuilder.AddCompanyDetails("Microsoft", 34, 100, 46);
+            empWageBuilder.AddCompanyDetails("Bridgelabz", 20, 130, 35);
+            empWageBuilder.IterateOverCompanies();
             Console.ReadLine();
         }
 
